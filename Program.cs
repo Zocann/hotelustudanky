7bfbdb1e918a19387a8d9e6be1defd676d1de275
Karel Mareth <mareth.karel@gmail.com>
2025-04-02 16:03:53 +0200

    Studanka-version-1

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using MudBlazor.Services;
+using Studanka.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Localization
+builder.Services.AddLocalization();
+builder.Services.AddControllers();
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+//Localization initialization
+string[] supportedCultures = ["cs-CZ", "en-US", "pl-PL"];
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapControllers();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
